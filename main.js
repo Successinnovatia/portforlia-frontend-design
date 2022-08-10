d43dc10ef563365578d1d48eceb88fb6aef820f6
Success Divine Onyechinemere <divinesuccess228@gmail.com>
2022-08-10 09:50:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function dropdownMenu(menu)
+{
+    var menu = document.getElementById(menu);
+    menu.classList.toggle('dropdown-show');
+}
+    
+function handleMenu()
+{
+    var navMenu = document.getElementById('sidebar');
+    navMenu.classList.toggle('open');
+}
